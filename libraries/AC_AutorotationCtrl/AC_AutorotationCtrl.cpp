384528f4a4932a61b040d864d1084724c5fc44b3
Gone4Dirt <matthewnkear@gmail.com>
2019-06-03 15:12:58 +0100

    AC_AUTOROTATIONCTRL: Setup new library.  HS/Col controller created.

---
+++ libraries/AC_AutorotationCtrl/AC_AutorotationCtrl.cpp
@@ -0,0 +1,260 @@
+#include "AC_AutorotationCtrl.h"
+
+
+const AP_Param::GroupInfo AC_AutorotationCtrl::var_info[] = {
+
+    // @Param: HS_P
+    // @DisplayName: P gain for head spead controller
+    // @Description: Increase value to increase sensitivity of head speed controller during autonomous autorotation.
+    // @Units: -
+    // @Range: 0.3 1
+    // @Increment: 0.01
+    // @User: Advanced
+    AP_GROUPINFO("HS_P", 0, AC_AutorotationCtrl, _param_hs_p, RPM_CONTROLLER_HEADSPEED_P),
+
+    // @Param: HS_D
+    // @DisplayName: D gain for head spead controller
+    // @Description: Increase value to increase damping of head speed controller during autonomous autorotation.
+    // @Units: -
+    // @Range: 0.001 0.02
+    // @Increment: 0.001
+    // @User: Advanced
+    AP_GROUPINFO("HS_D", 1, AC_AutorotationCtrl, _param_hs_d, RPM_CONTROLLER_HEADSPEED_D),
+    
+    // @Param: HS_I
+    // @DisplayName: I gain for head spead controller
+    // @Description: Increase value to increase damping of head speed controller during autonomous autorotation.
+    // @Units: -
+    // @Range: 0.001 0.02
+    // @Increment: 0.001
+    // @User: Advanced
+    AP_GROUPINFO("HS_I", 2, AC_AutorotationCtrl, _param_hs_i, RPM_CONTROLLER_HEADSPEED_I),
+    
+    // @Param: HS_I_LIM
+    // @DisplayName: I gain for head spead controller
+    // @Description: Increase value to increase damping of head speed controller during autonomous autorotation.
+    // @Units: -
+    // @Range: 0.001 0.02
+    // @Increment: 0.001
+    // @User: Advanced
+    AP_GROUPINFO("HS_I_LIM", 3, AC_AutorotationCtrl, _param_hs_i_lim, RPM_CONTROLLER_HEADSPEED_I_LIM),
+    
+    // @Param: HS_TARGET
+    // @DisplayName: Target head speed for controller to achieve
+    // @Description: Specify the target head speed as a percentage of hover headspeed
+    // @Units: -
+    // @Range: 0.8-1
+    // @Increment: 0.01
+    // @User: Advanced
+    AP_GROUPINFO("HS_TARGET", 4, AC_AutorotationCtrl, _param_target_head_speed, RPM_CONTROLLER_HEADSPEED_TARGET),
+    
+    // @Param: HS_HOVER
+    // @DisplayName: Temporary parameter for head speed in the hover
+    // @Description: This will be replaced by hover collective learning in the future
+    // @Units: -
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("HS_HOVER", 5, AC_AutorotationCtrl, _param_head_speed_hover, 1700.0f),
+    
+    // @Param: ENT_SLEW
+    // @DisplayName: The slew time for entry into headspeed/collective controller
+    // @Description: This allows a gentle transition into the contoller.  Specifiy the time to phase in the headspeed/collective controller PID.
+    // @Units: s
+    // @Range: 1-4
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("ENT_SLEW", 6, AC_AutorotationCtrl, _param_recovery_slew, RPM_CONTROLLER_ENTRY_SLEW_TIME),
+    
+    // @Param: ATT_P
+    // @DisplayName: P gain for head spead/airspeed attitude controller
+    // @Description: ---
+    // @Units: -
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("ATT_P", 7, AC_AutorotationCtrl, _param_hs_as_att_p, 2.0f),
+    
+    // @Param: ATT_I
+    // @DisplayName: I gain for head spead/airspeed attitude controller
+    // @Description: ---
+    // @Units: -
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("ATT_I", 8, AC_AutorotationCtrl, _param_hs_as_att_i, 2.0f),
+    
+    // @Param: ATT_I_LIM
+    // @DisplayName: I Limit for head spead/airspeed attitude controller
+    // @Description: ---
+    // @Units: -
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("ATT_I_LIM", 9, AC_AutorotationCtrl, _param_hs_as_att_i_lim, 2.0f),
+    
+    // @Param: ATT_D
+    // @DisplayName: D gain for head spead/airspeed attitude controller
+    // @Description: ---
+    // @Units: -
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("ATT_D", 10, AC_AutorotationCtrl, _param_hs_as_att_d, 2.0f),
+    
+    // @Param: TARG_AS
+    // @DisplayName: Target airspeed in cm/s for the autorotation controller to try and achieve/ maintain.
+    // @Description: ---
+    // @Units: cm/s
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("TARG_AS", 11, AC_AutorotationCtrl, _param_target_airspeed, 2.0f),
+    
+    // @Param: TD_ALT
+    // @DisplayName: Altitude at which to begin touch down phase
+    // @Description: ---
+    // @Units: cm
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("TD_ALT", 12, AC_AutorotationCtrl, _param_td_alt, 2.0f),
+    
+    // @Param: TD_COL_AGR
+    // @DisplayName: Collective agression during touch down phase
+    // @Description: ---
+    // @Units: cm
+    // @Range: -
+    // @Increment: -
+    // @User: Advanced
+    AP_GROUPINFO("TD_COL_AGR", 13, AC_AutorotationCtrl, _param_td_col_agression, 2.0f),
+    
+    AP_GROUPEND
+};
+
+
+void AC_AutorotationCtrl::update_hs_glide_controller(float dt)
+{
+    // Get singleton for RPM library
+    const AP_RPM *rpm = AP_RPM::get_instance();
+
+    //Get current rpm, checking to ensure no nullptr
+    if (rpm != nullptr) {
+        _current_rpm = rpm->get_rpm(0);
+    }
+    
+    //Use slew rate scaling for entry into phase
+    if (_flags.use_entry_slew_rate) {
+        _entry_slew_rate -= dt;
+        
+        if (_entry_slew_rate <= 0) {
+            //switch of entry slew
+            _flags.use_entry_slew_rate = 0;
+        }
+    }
+    
+    // ---- gain scaler calculation ----
+    float entry_gain_scaler = 1;  // By default no entry slew gain scaler to be used
+     
+    if (_flags.use_entry_slew_rate && _entry_slew_rate > 0) {
+        // Calculate entry slew gain scaler
+        entry_gain_scaler = (_param_recovery_slew - _entry_slew_rate) / _param_recovery_slew;
+    }
+
+    
+    
+    
+    //Prevent divide by zero error
+    if (_param_head_speed_hover < 500) {
+        _param_head_speed_hover = 500;  //Making sure that hover rpm is not unreasonably low
+    }
+    
+    //Calculate the head speed error
+    //Current rpm is normalised by the hover head speed.  Target head speed is defined as a percentage of hover speed
+    _head_speed_error = (_current_rpm / _param_head_speed_hover) - _param_target_head_speed;
+    
+    float P_hs = _head_speed_error * _param_hs_p * entry_gain_scaler;
+
+    //No I term to be used in entry to controller
+    float I_hs = 0;
+
+    if (!_flags.use_entry_slew_rate) {
+        //calculate integral of error
+        _error_integral += _head_speed_error;
+        
+        //apply I gain
+         I_hs = _error_integral * _param_hs_i;
+    }
+
+    
+    //check that I is within limits
+    if (I_hs < -_param_hs_i_lim) {
+        I_hs = -_param_hs_i_lim;
+    } else if (I_hs > _param_hs_i_lim) {
+        I_hs = _param_hs_i_lim;
+    }
+
+
+    //calculate head speed error differential
+    float head_speed_error_differential = (_head_speed_error - _last_head_speed_error) / dt;
+    
+    float D_hs = head_speed_error_differential * _param_hs_d * entry_gain_scaler;
+    
+    
+    
+    //Calculate collective position to be set
+    _collective_out = (P_hs + I_hs + D_hs) + _motors.get_throttle_hover();
+    
+    // send collective to setting to motors output library
+    set_collective(RPM_CONTROLLER_COLLECTIVE_CUTOFF_FREQ);
+    
+    //save last head speed error term for differential calculation in next time step
+    _last_head_speed_error = _head_speed_error;
+
+
+}
+
+
+
+
+void AC_AutorotationCtrl::set_collective(float collective_filter_cutoff)
+{
+// TODO: setup slew rate of collective application
+
+    _motors.set_throttle_filter_cutoff(collective_filter_cutoff);
+    _motors.set_throttle(_collective_out);
+
+}
+
+
+
+
+
+
+void AC_AutorotationCtrl::update_att_glide_controller(float dt)
+{
+
+    float aspeed;
+    if (_ahrs.airspeed_estimate(&aspeed)){
+    _airspeed_error = aspeed - _param_target_airspeed;
+    } else {
+    _airspeed_error = sqrtf(_inav.get_velocity().x * _inav.get_velocity().x  +  _inav.get_velocity().y * _inav.get_velocity().y  +  _inav.get_velocity().z * _inav.get_velocity().z);
+    }
+
+    float P_as = _airspeed_error * _param_hs_as_att_p;
+
+    _data = P_as;
+
+    float target_roll = 0;
+    float target_pitch = P_as;
+    float target_yaw_rate = 0;
+
+    _attitude_control.input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
+
+
+}
+
+
+
+
+
---
+++ libraries/AC_AutorotationCtrl/AC_AutorotationCtrl.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Param/AP_Param.h>
+#include <AP_Math/AP_Math.h>
+#include <AC_PID/AC_P.h>               // P library
+#include <AC_PID/AC_PID.h>             // PID library
+#include <AC_PID/AC_PI_2D.h>           // PI library (2-axis)
+#include <AC_PID/AC_PID_2D.h>          // PID library (2-axis)
+#include <AP_RPM/AP_RPM.h>
+#include <AP_Motors/AP_Motors.h>          // motors library
+#include <AC_AttitudeControl/AC_AttitudeControl.h> // Attitude control library
+#include <AP_InertialNav/AP_InertialNav.h>     // Inertial Navigation library
+
+
+// RPM controller default definitions
+#define RPM_CONTROLLER_COLLECTIVE_CUTOFF_FREQ          2.0f    // low-pass filter on accel error (unit: hz)
+#define COLLECTIVE_SLEW_RATE_ENTRY                     2.0f    // The time in seconds that it will take to initially set the collective during entry phase to match reversing of inflow direction (unit: s)
+#define RPM_CONTROLLER_HEADSPEED_P                     0.6f    // Defualt P gain for head speed controller (unit: -)
+#define RPM_CONTROLLER_HEADSPEED_I                     0.0015f // Defualt D gain for head speed controller (unit: -)
+#define RPM_CONTROLLER_HEADSPEED_D                     0.1f    // Defualt D gain for head speed controller (unit: -)
+#define RPM_CONTROLLER_HEADSPEED_I_LIM                 0.2f    // Defualt D gain for head speed controller (unit: -)
+#define RPM_CONTROLLER_HEADSPEED_TARGET                0.9f    // Defualt D gain for head speed controller (unit: -)
+#define RPM_CONTROLLER_ENTRY_SLEW_TIME                 2.0f    // Defualt D gain for head speed controller (unit: -)
+
+
+
+
+
+
+class AC_AutorotationCtrl
+{
+public:
+
+    /// Constructor
+AC_AutorotationCtrl(const AP_AHRS_View& ahrs,
+                     const AP_InertialNav& inav,
+                     AP_Motors& motors,
+                     AC_AttitudeControl& attitude_control) :
+    _ahrs(ahrs),
+    _inav(inav),
+    _motors(motors),
+    _attitude_control(attitude_control)
+    {
+        AP_Param::setup_object_defaults(this, var_info);
+    }
+
+
+
+    // Functions
+    void update_hs_glide_controller(float dt);  //Update head speed controller
+    void update_att_glide_controller(float dt);  //Update attitude controller
+    void set_collective(float _collective_filter_cutoff);
+
+
+    float get_data() { return _data; }
+    float get_rpm() { return _current_rpm; }
+    float get_rpm_error() { return _head_speed_error; }
+    
+    //To be called once when initiating the entry.  Calling multiple times will keep resetting the the slew rate timer.
+    void use_entry_slew() { _flags.use_entry_slew_rate = 1;  _entry_slew_rate = _param_recovery_slew; }
+    void set_attitude_hs_mixing_flag (bool flag_switch) { _flags.use_attitude_hs_mixing = flag_switch; }
+    void reset_I_terms() {_error_integral = 0.0f;}
+
+    float get_p() { return _param_hs_p; }
+    
+    float get_d() { return _param_hs_d; }
+    
+    float get_td_alt() { return _param_td_alt; }
+    
+    float get_td_agression() { return _param_td_col_agression; }
+
+    
+    // User settable parameters
+    static const struct AP_Param::GroupInfo var_info[];
+
+
+protected:
+
+    //----------------------------
+    //---- internal variables ----
+    //----------------------------
+    //Head Speed / Collective Controller
+    float _current_rpm;
+    float _collective_out;
+    float _entry_slew_rate;  //(s) Number of seconds to apply collective setting over
+    float _head_speed_error;  // (rad/s) error between target head speed and current head speed
+    float _error_integral;
+    float _last_head_speed_error;
+    
+    //Head Speed / Attitude Controller
+    float _airspeed_error;
+    
+    float _data;
+    
+    
+    //internal flags
+    struct rpm_controller_flags {
+            bool use_attitude_hs_mixing     : 1;    // 1 if attitude/collective mixing should be used to control head speed
+            bool use_entry_slew_rate        : 1;    // 1 if the phase of flight requires a gradual slew from one collective position to another
+    } _flags;
+    
+    // Parameter values
+    AP_Float _param_hs_p;               //p value for rpm collective controller
+    AP_Float _param_hs_d;               //d value for rpm collective controller
+    AP_Float _param_hs_i;               //i value for rpm collective controller
+    AP_Float _param_hs_i_lim;           //i limit value for rpm collective controller
+    AP_Float _param_target_head_speed;  //(rpm) normalised head speed to be maintined in the autorotation (normalised by hover head speed)
+    AP_Float _param_head_speed_hover;   //(rpm) head speed during normal hover
+    AP_Float _param_recovery_slew;      //(s) the time period over which the PD errors are phased in and the I terms are switched off
+    AP_Float _param_hs_as_att_p;
+    AP_Float _param_hs_as_att_i;
+    AP_Float _param_hs_as_att_i_lim;
+    AP_Float _param_hs_as_att_d;
+    AP_Float _param_target_airspeed;
+    AP_Float _param_td_alt;
+    AP_Float _param_td_col_agression;
+    
+    
+    
+    
+    // References to other libraries
+    const AP_AHRS_View&         _ahrs;
+    const AP_InertialNav&       _inav;
+    AP_Motors&                  _motors;
+    AC_AttitudeControl&         _attitude_control;
+    
+    
+    
+    
+};
