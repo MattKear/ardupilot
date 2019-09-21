4964e287d13730de5de7ff7dce61411bb81a5eb5
Gone4Dirt <matthewnkear@gmail.com>
2019-09-21 22:37:57 +0100

    AC_Autorotation: Created autorotation library

---
+++ libraries/AC_Autorotation/AC_Autorotation.cpp
@@ -0,0 +1,237 @@
+#include "AC_Autorotation.h"
+
+const AP_Param::GroupInfo AC_Autorotation::var_info[] = {
+
+    // @Param: ENABLE
+    // @DisplayName: Enable settings for RSC Setpoint
+    // @Description: Allows you to enable (1) or disable (0) the autonomous autorotation capability.
+    // @Values: 0:Disabled,1:Enabled
+    // @User: Advanced
+    AP_GROUPINFO_FLAGS("ENABLE", 0, AC_Autorotation, _param_enable, 0, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: HS_P
+    // @DisplayName: P gain for head spead controller
+    // @Description: Increase value to increase sensitivity of head speed controller during autonomous autorotation.
+    // @Range: 0.3 1
+    // @Increment: 0.01
+    // @User: Advanced
+    AP_SUBGROUPINFO(_p_hs, "HS_", 1, AC_Autorotation, AC_P),
+
+    // @Param: HS_SET_PT
+    // @DisplayName: Target Head Speed
+    // @Description: The target head speed in RPM during autorotation.  Start by setting to desired hover speed and tune from there as neccassary.
+    // @Units: RPM
+    // @Range: 1000 2800
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("HS_SET_PT", 2, AC_Autorotation, _param_head_speed_set_point, 1500.0f),
+
+    // @Param: TARG_SP
+    // @DisplayName: Target Glide Ground Speed
+    // @Description: Target ground speed in cm/s for the autorotation controller to try and achieve/ maintain during the glide phase.
+    // @Units: cm/s
+    // @Range: 800 2000
+    // @Increment: 50
+    // @User: Advanced
+    AP_GROUPINFO("TARG_SP", 3, AC_Autorotation, _param_target_speed, SPD_HGT_CONTROLLER_GND_SPEED_TARGET),
+
+    // @Param: COL_FILT_E
+    // @DisplayName: Entry Phase Collective Filter
+    // @Description: Cut-off frequency for collective low pass filter.  For the entry phase.  Acts as a following trim.  Must be higher than COL_FILT_G.
+    // @Units: Hz
+    // @Range: 0.2 0.5
+    // @Increment: 0.01
+    // @User: Advanced
+    AP_GROUPINFO("COL_FILT_E", 4, AC_Autorotation, _param_col_entry_cutoff_freq, HS_CONTROLLER_ENTRY_COL_FLITER),
+
+    // @Param: COL_FILT_G
+    // @DisplayName: Glide Phase Collective Filter
+    // @Description: Cut-off frequency for collective low pass filter.  For the glide phase.  Acts as a following trim.  Must be lower than COL_FILT_E.
+    // @Units: Hz
+    // @Range: 0.03 0.15
+    // @Increment: 0.01
+    // @User: Advanced
+    AP_GROUPINFO("COL_FILT_G", 5, AC_Autorotation, _param_col_glide_cutoff_freq, HS_CONTROLLER_GLIDE_COL_FLITER),
+
+    // @Param: AS_ACC_MAX
+    // @DisplayName: Forward Acceleration Limit
+    // @Description: Maximum forward acceleration to apply in speed controller.
+    // @Units: cm/s/s
+    // @Range: 50 200
+    // @Increment: 10
+    // @User: Advanced
+    AP_GROUPINFO("AS_ACC_MAX", 6, AC_Autorotation, _param_accel_max, SPD_HGT_CONTROLLER_MAX_ACCEL),
+
+    // @Param: BAIL_TIME
+    // @DisplayName: Bail Out Timer
+    // @Description: Time in seconds from bail out initiated to the exit of autorotation flight mode.
+    // @Units: s
+    // @Range: 0.5 4
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("BAIL_TIME", 7, AC_Autorotation, _param_bail_time, AROT_BAIL_OUT_TIME),
+
+    // @Param: HS_SENSOR
+    // @DisplayName: Main Rotor RPM Sensor 
+    // @Description: Allocate the RPM sensor instance to use for measuring head speed.  RPM1 = 0.  RPM2 = 1.
+    // @Units: s
+    // @Range: 0.5 3
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("HS_SENSOR", 8, AC_Autorotation, _param_rpm_instance, 0),
+
+    AP_GROUPEND
+};
+
+
+//initialisation of head speed controller
+void AC_Autorotation::init_hs_controller()
+{
+    //set initial collective position to be the collective position on initialisation
+    _collective_out = 0.4f;//_motors.get_throttle();
+
+    //Reset feed forward filter
+    col_trim_lpf.reset(_collective_out);
+
+    //reset flags
+    _flags.bad_rpm = false;
+
+    //reset RPM health monitoring
+    _unhealthy_rpm_counter = 0;
+    _healthy_rpm_counter = 0;
+}
+
+
+bool AC_Autorotation::update_hs_glide_controller(float dt)
+{
+    //reset rpm health flag
+    _flags.bad_rpm = false;
+    _flags.bad_rpm_warning = false;
+
+    //get current rpm and update healthly signal counters
+    _current_rpm = get_rpm(true);
+
+    if (_unhealthy_rpm_counter <=30) {
+        //Normalised head speed
+        float head_speed_norm = _current_rpm / _param_head_speed_set_point;
+
+        //set collective trim low pass filter cut off frequency
+        col_trim_lpf.set_cutoff_frequency(_col_cutoff_freq);
+
+        //Calculate the head speed error
+        //Current rpm is normalised by the set point head speed.  Target head speed is defined as a percentage of the set point.
+        _head_speed_error = head_speed_norm - _target_head_speed;
+
+        _p_term_hs = _p_hs.get_p(_head_speed_error);
+
+        //Adjusting collective trim using feed forward
+        _ff_term_hs = col_trim_lpf.apply(_collective_out, dt); //note that the collective out has not yet been updated, so this value is the previous time steps collective position
+
+        //Calculate collective position to be set
+        _collective_out = _p_term_hs + _ff_term_hs;
+
+    } else {
+        //RPM sensor is bad set collective to minimum
+        _collective_out = -1.0f;
+
+        _flags.bad_rpm_warning = true;
+    }
+
+    // send collective to setting to motors output library
+    set_collective(HS_CONTROLLER_COLLECTIVE_CUTOFF_FREQ);
+
+    return _flags.bad_rpm_warning;
+}
+
+
+//function to set collective and collective filter in motor library
+void AC_Autorotation::set_collective(float collective_filter_cutoff)
+{
+    _motors.set_throttle_filter_cutoff(collective_filter_cutoff);
+    _motors.set_throttle(_collective_out);
+}
+
+
+//function that sets parameter values in flight mode
+void AC_Autorotation::set_param_values(int16_t* set_point_hs, int16_t* accel, int16_t* targ_s, float* td_alt, float* ent_freq, float* glide_freq, float* bail_time)
+{
+    *set_point_hs   = _param_head_speed_set_point;
+    *accel          = _param_accel_max;
+    *targ_s         = _param_target_speed;
+    *td_alt         = _param_td_alt;
+    *ent_freq       = _param_col_entry_cutoff_freq;
+    *glide_freq     = _param_col_glide_cutoff_freq;
+    *bail_time      = _param_bail_time;
+}
+
+
+//function that gets rpm and does rpm signal checking to ensure signal is reliable
+//before using it in the controller
+float AC_Autorotation::get_rpm(bool update_counter)
+{
+    // Get singleton for RPM library
+    const AP_RPM *rpm = AP_RPM::get_singleton();
+
+    float current_rpm = 0.0f;
+
+    //Get current rpm, checking to ensure no nullptr
+    if (rpm != nullptr) {
+        //Check requested rpm instance to ensure either 0 or 1.  Always defaults to 0.
+        if (_param_rpm_instance > 1) {
+            _param_rpm_instance = 0;
+        } else if (_param_rpm_instance < 0) {
+            _param_rpm_instance = 0;
+        }
+
+        //Get RPM value
+        uint8_t instance = _param_rpm_instance;
+        current_rpm = rpm->get_rpm(instance);
+
+        //Check RPM sesnor is returning a healthy status
+        if (current_rpm <= -1) {
+            //unhealthy, rpm unreliable
+            _flags.bad_rpm = true;
+        }
+
+    } else {
+        _flags.bad_rpm = true;
+    }
+
+    if (_flags.bad_rpm) {
+        //count unhealthy rpm updates and reset healthy rpm counter
+        _unhealthy_rpm_counter++;
+        _healthy_rpm_counter = 0;
+
+    } else if (!_flags.bad_rpm && _unhealthy_rpm_counter > 0) {
+        //poor rpm reading may have cleared.  Wait 10 update cycles to clear.
+        _healthy_rpm_counter++;
+
+        if (_healthy_rpm_counter >= 10) {
+            //poor rpm health has cleared, reset counters
+            _unhealthy_rpm_counter = 0;
+            _healthy_rpm_counter = 0;
+        }
+    }
+
+    //update data flash logs
+    update_logger();
+
+    return current_rpm;
+}
+
+
+void AC_Autorotation::update_logger(void)
+{
+    //Write to data flash log
+    if (_log_counter++ % 20 == 0) {
+        AP::logger().Write("AROT",
+                           "TimeUS,P,hserr,ColOut,FFCol,CRPM",
+                           "Qfffff",
+                           AP_HAL::micros64(),
+                           (double)_p_term_hs,
+                           (double)_head_speed_error,
+                           (double)_collective_out,
+                           (double)_ff_term_hs,
+                           (double)_current_rpm);
+    }
+}
---
+++ libraries/AC_Autorotation/AC_Autorotation.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_Param/AP_Param.h>
+#include <AP_Math/AP_Math.h>
+#include <AP_Logger/AP_Logger.h>
+#include <AP_RPM/AP_RPM.h>
+#include <AP_Motors/AP_Motors.h>
+#include <AP_Motors/AP_MotorsHeli_RSC.h>
+#include <AC_AttitudeControl/AC_AttitudeControl.h>
+#include <AP_InertialNav/AP_InertialNav.h>
+#include <Filter/Filter.h>
+#include <Filter/LowPassFilter.h>
+#include <AC_PID/AC_P.h>
+
+//Autorotation controller defaults
+#define AROT_BAIL_OUT_TIME                            2.0f     // Default time for bail out controller to run (unit: s)
+#define HEAD_SPEED_TARGET_RATIO                       1.0f     // Normalised target main rotor head speed (unit: -)
+
+// Head Speed (HS) controller specific default definitions
+#define HS_CONTROLLER_COLLECTIVE_CUTOFF_FREQ          2.0f     // low-pass filter on accel error (unit: hz)
+#define HS_CONTROLLER_HEADSPEED_P                     0.7f     // Default P gain for head speed controller (unit: -)
+#define HS_CONTROLLER_ENTRY_COL_FLITER                0.35f    // Default low pass filter frequency during the entry phase (unit: Hz)
+#define HS_CONTROLLER_GLIDE_COL_FLITER                0.05f    // Default low pass filter frequency during the glide phase (unit: Hz)
+
+// Speed Height controller specific default definitions for autorotation use
+#define SPD_HGT_CONTROLLER_GND_SPEED_TARGET           1100.0f  // Default target ground speed for speed height controller (unit: cm/s)
+#define SPD_HGT_CONTROLLER_MAX_ACCEL                  100.0f   // Default acceleration limit for speed height controller (unit: cm/s/s)
+
+class AC_Autorotation
+{
+public:
+
+    /// Constructor
+AC_Autorotation(const AP_AHRS_View& ahrs,
+                     const AP_InertialNav& inav,
+                     AP_Motors& motors,
+                     AC_AttitudeControl& attitude_control) :
+    _ahrs(ahrs),
+    _inav(inav),
+    _motors(motors),
+    _attitude_control(attitude_control),
+    _p_hs(HS_CONTROLLER_HEADSPEED_P)
+    {
+        AP_Param::setup_object_defaults(this, var_info);
+    }
+
+    //--------Functions--------
+    void init_hs_controller(void);  //Initialise head speed controller
+    bool update_hs_glide_controller(float dt);  //Update head speed controller
+    float get_rpm(void) { return _current_rpm; }  //Function just returns the rpm as last read in this library
+    float get_rpm(bool update_counter);  //Function fetches fresh rpm update and continues sensor health monitoring
+    void set_target_head_speed(float ths) { _target_head_speed = ths; }  //Sets the normalised target head speed
+    void set_col_cutoff_freq(float freq) { _col_cutoff_freq = freq; }  //Sets the collective low pass filter cut off frequency
+    void set_param_values(int16_t* set_point_hs, int16_t* accel, int16_t* targ_s, float* td_alt, float* ent_freq, float* glide_freq, float* bail_time);  //Enables the parameter values to be retrieved by the autorotation flight mode
+    float get_last_collective() { return _collective_out; }
+    bool is_enable(void) { return _param_enable; }
+
+    // User Settable Parameters
+    static const struct AP_Param::GroupInfo var_info[];
+
+
+protected:
+
+    //--------Internal Variables--------
+    float _current_rpm;
+    float _collective_out = 0.0f;
+    float _head_speed_error;         //Error between target head speed and current head speed.  Normalised by head speed set point RPM.
+    uint16_t _log_counter = 0;
+    float _col_cutoff_freq;          //Lowpass filter cutoff frequency (Hz) for collective
+    uint8_t _unhealthy_rpm_counter;  //Counter used to track RPM sensor unhealthy signal
+    uint8_t _healthy_rpm_counter;    //Counter used to track RPM sensor healthy signal
+    float _target_head_speed;        //Normalised target head speed.  Normalised by head speed set point RPM.
+    float _p_term_hs;                //Proportional contribution to collective setting
+    float _ff_term_hs;               //Following trim feed forward contribution to collective setting
+
+    //--------Parameter Values--------
+    AC_P _p_hs;
+    AP_Int8  _param_enable;
+    AP_Float _param_hs_p;
+    AP_Int16 _param_head_speed_set_point;
+    AP_Int16 _param_accel_max;
+    AP_Int16 _param_target_speed;
+    AP_Float _param_col_entry_cutoff_freq;
+    AP_Float _param_col_glide_cutoff_freq;
+    AP_Float _param_bail_time;
+    AP_Int8  _param_rpm_instance;
+
+    AP_Float _param_td_alt;  //currently no parameter.  Will be used in future work.
+
+    //--------Internal Flags--------
+    struct controller_flags {
+            bool bad_rpm             : 1;
+            bool bad_rpm_warning     : 1;
+    } _flags;
+
+    //--------Internal Functions--------
+    void update_logger(void);
+    void set_collective(float _collective_filter_cutoff);
+
+    // low pass filter for collective trim
+    LowPassFilterFloat col_trim_lpf;
+
+    //--------References to Other Libraries--------
+    const AP_AHRS_View&         _ahrs;
+    const AP_InertialNav&       _inav;
+    AP_Motors&                  _motors;
+    AC_AttitudeControl&         _attitude_control;
+
+};
