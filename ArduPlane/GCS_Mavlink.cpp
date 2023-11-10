30a2e9e29a7b446e867c7ca38be5724e28ad718d
Gone4Dirt <matthewnkear@gmail.com>
2023-11-10 15:31:06 +0000

    Plane: Allow DO_CHANGE_SPEED in RTL

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -1095,7 +1095,8 @@ MAV_RESULT GCS_MAVLINK_Plane::handle_command_DO_CHANGE_SPEED(const mavlink_comma
         // this command should be ignored since it comes in from GCS
         // or a companion computer:
         if ((!plane.control_mode->is_guided_mode()) &&
-            (plane.control_mode != &plane.mode_auto)) {
+            (plane.control_mode != &plane.mode_auto) && 
+            (plane.control_mode != &plane.mode_rtl)) {
             // failed
             return MAV_RESULT_FAILED;
         }
