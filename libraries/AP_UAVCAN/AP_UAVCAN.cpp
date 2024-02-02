472c9a157265043af20a0855e30202a75f1f218e
Iampete1 <iampete@hotmail.co.uk>
2024-02-02 13:34:01 +0000

    AP_UAVCAN: populate ESC power percentage

--- libraries/AP_UAVCAN/AP_UAVCAN.cpp
+++ libraries/AP_UAVCAN/AP_UAVCAN.cpp
@@ -1509,13 +1509,15 @@ void AP_UAVCAN::handle_ESC_status(AP_UAVCAN* ap_uavcan, uint8_t node_id, const E
         .temperature_cdeg = int16_t((KELVIN_TO_C(cb.msg->temperature)) * 100),
         .voltage = cb.msg->voltage,
         .current = cb.msg->current,
+        .power_percentage = cb.msg->power_rating_pct,
     };
 
     ap_uavcan->update_rpm(esc_index, cb.msg->rpm, cb.msg->error_count);
     ap_uavcan->update_telem_data(esc_index, t,
         (isnan(cb.msg->current) ? 0 : AP_ESC_Telem_Backend::TelemetryType::CURRENT)
             | (isnan(cb.msg->voltage) ? 0 : AP_ESC_Telem_Backend::TelemetryType::VOLTAGE)
-            | (isnan(cb.msg->temperature) ? 0 : AP_ESC_Telem_Backend::TelemetryType::TEMPERATURE));
+            | (isnan(cb.msg->temperature) ? 0 : AP_ESC_Telem_Backend::TelemetryType::TEMPERATURE)
+            | (isnan(cb.msg->power_rating_pct) ? 0 : AP_ESC_Telem_Backend::TelemetryType::POWER_PERCENTAGE));
 #endif
 }
 
