3835ce2de25df87156d332f943b33c78fff671b7
Dan <50102283+Dan-Moss@users.noreply.github.com>
2024-03-11 11:44:46 +0000

    Common: scaling factor fix

--- libraries/AP_Common/Location.cpp
+++ libraries/AP_Common/Location.cpp
@@ -302,8 +302,8 @@ Vector3f Location::get_distance_NED_alt_frame(const Location &loc2) const
         alt1 = 0, alt2 = 0;
     }
     return Vector3f((loc2.lat - lat) * LOCATION_SCALING_FACTOR,
-                    diff_longitude(loc2.lng,lng) * LOCATION_SCALING_FACTOR * longitude_scale(loc2.lat+lat/2),
-                    (alt1 - alt2) * 0.01f);
+                    diff_longitude(loc2.lng,lng) * LOCATION_SCALING_FACTOR * longitude_scale((loc2.lat+lat)/2),
+                    (alt1 - alt2) * 0.01);
 }
 
 // extrapolate latitude/longitude given distances (in meters) north and east
