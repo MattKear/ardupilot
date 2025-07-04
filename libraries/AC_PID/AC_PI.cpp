4fb3ca8ffd1d83e0e1dc5aaed616c1c594f9308e
MattKear <matthewnkear@gmail.com>
2025-07-04 03:25:07 +0100

    AC_PID: AC_PI: Add setting of integrator term and integrator limiting

--- libraries/AC_PID/AC_PI.cpp
+++ libraries/AC_PID/AC_PI.cpp
@@ -34,13 +34,50 @@ AC_PI::AC_PI(float initial_p, float initial_i, float initial_imax) :
     AP_Param::setup_object_defaults(this, var_info);
 }
 
-float AC_PI::update(float measurement, float target, float dt)
+float AC_PI::update(float measurement, float target, float dt, bool limit)
 {
-    const float err = target - measurement;
+    return update(measurement, target, dt, (limit && is_negative(integrator)), (limit && is_positive(integrator)));
+}
 
-    integrator += kI * err * dt;
-    integrator = constrain_float(integrator, 0, imax);
+float AC_PI::update(float measurement, float target, float dt, bool limit_neg, bool limit_pos)
+{
+    err = target - measurement;
+
+    update_i(dt, limit_neg, limit_pos);
+
     output_P = kP * err;
 
     return output_P + integrator;
+}
+
+//  update_i - update the integral
+//  if limit_neg is true, the integral can only increase
+//  if limit_pos is true, the integral can only decrease
+void AC_PI::update_i(float dt, bool limit_neg, bool limit_pos)
+{
+    if (!is_zero(kI.get())) {
+        // Ensure that integrator can only be reduced if the output is saturated
+        if (!((limit_neg && is_negative(err)) || (limit_pos && is_positive(err)))) {
+            integrator += (err * kI.get()) * dt;
+            integrator = constrain_float(integrator, -imax.get(), imax.get());
+        }
+    } else {
+        integrator = 0.0f;
+    }
+}
+
+void AC_PI::set_integrator(float target, float measurement, float i)
+{
+    set_integrator(target - measurement, i);
+}
+
+void AC_PI::set_integrator(float error, float i)
+{
+    err = error; // store error for logging
+    set_integrator(i - error * kP.get());
+}
+
+void AC_PI::set_integrator(float i)
+{
+    integrator = constrain_float(i, -imax.get(), imax.get());
 }
